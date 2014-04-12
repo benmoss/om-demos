7a33eea2d25767b5ddd74dd54905a0ede81873a3
Ben Moss <ben@mossity.com>
2014-04-12 16:56:50 -0400

    Kinda working

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [com.facebook/react "0.9.0.1"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [om "0.5.3"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
--- src/om_demos/core.cljs
+++ src/om_demos/core.cljs
@@ -1,38 +1,57 @@
 (ns om-demos.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [goog.date :as dt]))
+            [goog.date :as gdate]
+            [goog.events :as gevents]))
 
 (enable-console-print!)
 
-(def app-state (atom {:time (goog.date.DateTime.)}))
-
-(defn clock [data owner {:keys [time-key formatter tick-fn]}]
+(defn clock [data owner {:keys [formatter tick-fn interval]}]
   (reify
+    om/IInitState
+    (init-state [_]
+      {:animating false})
+    om/IWillUpdate
+    (will-update [_ next-props next-state]
+      (when (not= (om/get-props owner) next-props)
+        (.listenOnce goog.events (om/get-node owner "container") "webkitAnimationEnd" #(om/set-state! owner :animating false))
+        (om/set-state! owner :prior-val (om/get-props owner))
+        (om/set-state! owner :animating true)))
     om/IWillMount
     (will-mount [_]
-      (tick-fn data))
-    om/IRender
-    (render [_]
-      (dom/div nil
-               (formatter data)))))
+      (js/setInterval (partial tick-fn data) interval))
+    om/IRenderState
+    (render-state [_ state]
+      (let [prior (:prior-val state)
+            class-names (cond-> ["time"]
+                          (:animating state) (conj "animating"))
+            class-names (apply str (interpose " " class-names))]
+        (dom/div #js {:className class-names}
+                 (dom/div #js {:ref "container"}
+                          (dom/div #js {:className "next"} (formatter data))
+                          (dom/div #js {:className "current"} (when prior (formatter prior)))))))))
 
 (defn formatter [data]
   (.toIsoTimeString (:time data)))
 
 (defn tick [data]
-  (js/setInterval
-    (fn [] (om/update! data {:time (doto (:time @data)
-                                     (.add (goog.date.Interval. 0 0 0 0 0 1)))}))
-    1000))
+  (om/update! data {:time (doto (goog.date.DateTime. (:time @data))
+                            (.add (goog.date.Interval. 0 0 0 0 0 1)))}))
+
+(defn app-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "topnav"}
+      (dom/div #js {:id "main"}
+      (dom/div #js {:id "now-demo"}
+               (om/build clock app {:opts {:interval 1000
+                                           :formatter formatter
+                                           :tick-fn tick}})))))))
+
+(def app-state (atom {:time (goog.date.DateTime.)}))
 
 (om/root
-  (fn [app owner]
-    (reify
-      om/IRender
-      (render [_]
-        (om/build clock app {:opts {:time-key :time
-                                    :formatter formatter
-                                    :tick-fn tick}}))))
+  app-view
   app-state
   {:target (. js/document (getElementById "app"))})
