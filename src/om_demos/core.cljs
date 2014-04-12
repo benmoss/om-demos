5fa8532733f93fc0c233ebb19ab86f8946b7cce3
Ben Moss <ben@mossity.com>
2014-04-12 12:07:00 -0400

    Slightly broken clock demo

--- src/om_demos/core.cljs
+++ src/om_demos/core.cljs
@@ -1,13 +1,35 @@
 (ns om-demos.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [goog.date :as dt]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:time (goog.date.DateTime.)}))
+
+(defn clock [data owner {:keys [time-key formatter tick-fn]}]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (tick-fn data))
+    om/IRender
+    (render [_]
+      (dom/div nil
+               (formatter data)))))
+
+(defn formatter [data]
+  (.toIsoTimeString (:time data)))
+
+(defn tick [data]
+  (js/setInterval
+    (fn [] (om/update! data {:time (doto (:time @data)
+                                     (.add (goog.date.Interval. 0 0 0 0 0 1)))}))
+    1000))
 
 (om/root
   (fn [app owner]
-    (dom/h1 nil (:text app)))
+    (om/build clock app {:opts {:time-key :time
+                                :formatter formatter
+                                :tick-fn tick}}))
   app-state
   {:target (. js/document (getElementById "app"))})
